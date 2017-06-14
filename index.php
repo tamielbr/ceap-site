794cce9c368909e90f8fd83763b0e5fa0e0e3fd2
Glauber Modolo Cabral <glauber.sp@gmail.com>
2017-06-14 17:14:22 -0300

    Colocada página do encontro 2017.

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+        crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="site/public/css/ceap.css" charset="utf-8">
     <link rel="icon" type="image/png" href="site/public/img/favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="site/public/img/favicon-16x16.png" sizes="16x16" />
@@ -16,23 +17,34 @@
 
 <body>
     <div class="container" style="margin-top:50px">
-      <div class="jumbotron" style="text-align:center">
-          <div style="display: flex;align-items: center;justify-content: center;">
-              <img alt="Logotipo" src="site/public/img/logo_escolas.png">
-              <h1>Projeto CEAP</h1>
-          </div>
-          <p>Melhorando as condições de Ensino, Aprendizagem e Participação na Escola</p>
-          <div class="row">
-              <div class="col-md-4 col-sm-4" style="margin-bottom: 10px;"><a class="btn btn-primary btn-lg" href="site/sistemas.php" role="button">Sistemas</a></div>
-              <div class="col-md-4 col-sm-4" style="margin-bottom: 10px;"><a class="btn btn-primary btn-lg" href="site/sobre.php" role="button">Sobre o projeto</a></div>
-              <div class="col-md-4 col-sm-4" style="margin-bottom: 10px;"><a class="btn btn-primary btn-lg" href="site/contato.php" role="button">Contato</a></div>
-          </div>
+        <div class="jumbotron" style="text-align:center">
+            <div style="display: flex;align-items: center;justify-content: center;">
+                <img alt="Logotipo" src="site/public/img/logo_escolas.png">
+                <h1>Projeto CEAP</h1>
+            </div>
+            <p>Melhorando as condições de Ensino, Aprendizagem e Participação na Escola</p>
+            <div class="row">
+                <div class="col-md-4 col-sm-4" style="margin-bottom: 10px;"><a class="btn btn-primary btn-lg" href="site/sistemas.php" role="button">Sistemas</a></div>
+                <div class="col-md-4 col-sm-4" style="margin-bottom: 10px;"><a class="btn btn-primary btn-lg" href="site/sobre.php" role="button">Sobre o projeto</a></div>
+                <div class="col-md-4 col-sm-4" style="margin-bottom: 10px;"><a class="btn btn-primary btn-lg" href="site/contato.php" role="button">Contato</a></div>
+            </div>
+        </div>
+        <div class="jumbotron">
+            <div class="row">
+                <div class="col-md-12 col-sm-12" style="display: flex;align-items: center;justify-content: center;margin-bottom: 10px;">
+                    <h1>Encontro CEAP 2017</h1>
+                </div>
+                <div class="col-md-12 col-sm-12" style="display: flex;align-items: center;justify-content: center;margin-bottom: 10px;">
+                    <p><a class="btn btn-primary btn-lg" href="site/encontro.html" role="button">Saiba mais</a></p>
+                </div>
+            </div>
         </div>
     </div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
