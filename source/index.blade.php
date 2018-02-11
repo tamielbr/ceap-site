8fde003f3969e23efd353b73930124e8befa0e2e
Tel Amiel <tel@amiel.info>
2018-02-11 17:14:46 -0200

    Mudanças de texto
    Mudando descrição do box para SAE (ao invés de Voluta)

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,13 +2,13 @@
 <div class="container" style="margin-top:50px">
     <div class="jumbotron" style="text-align:center">
         <div style="display: flex;align-items: center;justify-content: center;">
-            <img alt="Logotipo" src="{{ $page->baseUrl.'/img/logo_escolas.png' }}">
-            <h1>VOLUTA</h1>
+            <!-- <img alt="Logotipo" src="{{ $page->baseUrl.'/img/logo_escolas.png' }}"> -->
+            <h1>Sistema de Acompanhamento de Ações Escolares</h1>
         </div>
-        <p>Melhorando as condições de Ensino, Aprendizagem e Participação na Escola</p>
+        <p>Apoiando escolas no planejamento e monitoramento coletivo de ações e tomada de decisões</p>
         <div class="row">
-            <div class="col-md-4 col-sm-4" style="margin-bottom: 10px;"><a class="btn btn-primary btn-lg" href="{{ $page->baseUrl.'/sae' }}" role="button">SAE</a></div>
-            <div class="col-md-4 col-sm-4" style="margin-bottom: 10px;"><a class="btn btn-primary btn-lg" href="{{ $page->baseUrl.'/sobre' }}" role="button">Sobre</a></div>
+            <div class="col-md-4 col-sm-4" style="margin-bottom: 10px;"><a class="btn btn-primary btn-lg" href="{{ $page->baseUrl.'/sae' }}" role="button">Acesso</a></div>
+            <div class="col-md-4 col-sm-4" style="margin-bottom: 10px;"><a class="btn btn-primary btn-lg" href="{{ $page->baseUrl.'/sobre' }}" role="button">Sobre o SAE</a></div>
             <div class="col-md-4 col-sm-4" style="margin-bottom: 10px;"><a class="btn btn-primary btn-lg" href="{{ $page->baseUrl.'/tutoriais' }}" role="button">Tutoriais</a></div>
         </div>
     </div>
