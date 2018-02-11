f63d51da2349416b0c1820404c03e006ba7898e4
Tel Amiel <tel@amiel.info>
2018-02-11 17:19:08 -0200

    Mudanças no "sobre" SAE

--- source/sobre.blade.php
+++ source/sobre.blade.php
@@ -9,12 +9,11 @@
                     <h1 class="panel-title">Sobre o projeto</h1>
                 </div>
                 <div class="panel-body">
-                    <h2 style="margin-top:0">Projeto CEAP</h2>
-                    <p>Este projeto tem como objetivo implementar e investigar uma metodologia para a melhoria da escola pública.
-                    </p>
-                    <p>Partimos do conceito de uma noção sistêmica do "clima escolar" avaliando e buscando aprimorar as condições de aprendizado, ensino e participação da comunidade escolar, promovendo uma maior abertura à transformações e inovação.</p>
+                    <h2 style="margin-top:0">Sobre o SAE</h2>
+                    <p>“O Sistema de Acompanhamento de Ações Escolares (SAE) é resultado do Projeto CEAP (melhorando as condições de Ensino, Aprendizagem e Participação na Escola), financiado pela FAPESP (2015-2018) e construído no <a href="http://nied.unicamp.br">NIED/Unicamp</a>, que teve como objetivo implementar e investigar uma metodologia para a melhoria da escola pública.</p>
+                    <p>No Projeto CEAP, partimos do conceito de uma noção sistêmica do "clima escolar" avaliando e buscando aprimorar as condições de aprendizado, ensino e participação da comunidade escolar, promovendo uma maior abertura à transformações e inovação.</p>
                     <p>Para avaliar essas condições, fazemos uso de questionários preenchidos semestralmente online por todos os entes escolares (pais, alunos, gestores, e funcionários) sobre sua percepção de quatro fatores que contribuem para um bom clima
-                        escolar.</p>
+                        escolar, feito em parceria com a <a href="http://tetraanalytix.com">Tetra Analytix</a></p>
                     <p>Envolvemos todos os atores na discussão e apreciação dos dados para definições de metas e ações para a escola. Como parte do projeto, desenvolvemos material formação, registro e tomada de decisões, bem como um sistema de software para
                         gestão desses processos.</p>
                     <p>Para saber mais, leia um <a href="{{ $page->baseUrl.'/ceap_sobre_curto.pdf' }}" target="_blank">breve artigo sobre o Projeto CEAP.</a></p>
@@ -33,11 +32,11 @@
 
                     <h2>Equipe</h2>
                     <ul>
-                        <li>Pesq. Dr. Tel Amiel - Pesquisador do Núcleo de Informática Aplicada à Educação <a href="http://lattes.cnpq.br/0246540741711761" target="_blank">(Plataforma Lattes)</a></li>
+                        <li>Pesq. Dr. Tel Amiel - Professor da FE/UnB (anteriormente, pesquisador do Núcleo de Informática Aplicada à Educação) <a href="http://lattes.cnpq.br/0246540741711761" target="_blank">(Plataforma Lattes)</a></li>
                         <li>Me. Wilmara Alves Thomaz - Professora da Rede Estadual de São Paulo e Bolsista FAPESP <a href="http://lattes.cnpq.br/8509146425967503"  target="_blank">(Plataforma Lattes)</a></li>
                         <li>Me. Dulcinéia Aparecida Ribeiro - Professora da Rede Municipal de Campinas, Diretora Escolar da Rede Estadual de São Paulo e Bolsista FAPESP <a href="http://lattes.cnpq.br/7719673938922296"  target="_blank">(Plataforma Lattes)</a></li>
-                        <li>Me. Glauber Modolo Cabral (Bolsista FAPESP) <a href="http://lattes.cnpq.br/1756949876810254"  target="_blank">(Plataforma Lattes)</a></li>
-                        <li>Cícero Alex (Colaborador) <a href="http://lattes.cnpq.br/2841026863780814"  target="_blank">(Plataforma Lattes)</a></li></li>
+                        <li>Me. Glauber Modolo Cabral (Bolsista TT/FAPESP) <a href="http://lattes.cnpq.br/1756949876810254"  target="_blank">(Plataforma Lattes)</a></li>
+                        <li>Cícero Alex (Bolsista TT/FAPESP)<a href="http://lattes.cnpq.br/2841026863780814"  target="_blank">(Plataforma Lattes)</a></li></li>
                     </ul>
 
                     <h2>Informações adicionais</h2>
